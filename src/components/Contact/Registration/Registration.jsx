e2cb10609b024e763e0e776542dd182658464bfb
Ajmain Nishu <ajmainnishu@gmail.com>
2023-10-15 03:29:00 +0600

    feat: Registration component added

--- src/components/Contact/Registration/Registration.jsx
+++ src/components/Contact/Registration/Registration.jsx
@@ -1,7 +1,42 @@
 import { Link } from 'react-router-dom';
 import logo from '../../../assets/logo/logo.png';
+import Swal from 'sweetalert2';
+import { ToastContainer, toast } from 'react-toastify';
 
 const Registration = () => {
+    // register button
+    const handleRegister = event => {
+        event.preventDefault()
+        const form = event.target;
+        const name = form.name.value;
+        const email = form.email.value;
+        const date = form.date.value;
+        const description = form.description.value;
+        const organize = form.organize.value;
+        const register = { name, email, date, description, organize }
+        // register post data
+        fetch('http://localhost:5000/register', {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(register)
+        })
+            .then(() => {
+                // sweetalert
+                Swal.fire({
+                    position: 'center',
+                    icon: 'success',
+                    title: 'Register Done',
+                    showConfirmButton: true,
+                    timer: 1500
+                })
+                form.reset();
+            }).catch(error => {
+                // toastify
+                toast(error.message);
+            })
+    }
     return (
         <div className='pt-11 pb-44 space-y-7'>
             <Link to={`/`} className=''>
@@ -9,27 +44,34 @@ const Registration = () => {
             </Link>
             <div className='space-y-12 pt-9 px-10 md:px-14 pb-16 border border-[#ABABAB] rounded md:w-[570px] md:mx-auto mx-5'>
                 <h2 className='text-2xl font-bold text-black'>Register as a Volunteer</h2>
-                <form className="card-bod space-y-9">
+                <form onSubmit={handleRegister} className="card-bod space-y-9">
+                    {/* full name */}
                     <div className="form-control">
-                        <input type="text" placeholder="Full Name" className="input input-bordered border-t-0 border-e-0 border-s-0 border-b-[#C5C5C5] rounded-none ps-0 placeholder:text-black focus:outline-none placeholder:font-medium" />
+                        <input type="text" name="name" placeholder="Full Name" className="input input-bordered border-t-0 border-e-0 border-s-0 border-b-[#C5C5C5] rounded-none ps-0 placeholder:text-black focus:outline-none placeholder:font-medium" required />
                     </div>
+                    {/* email */}
                     <div className="form-control">
-                        <input type="email" placeholder="Email" className="input input-bordered border-t-0 border-e-0 border-s-0 border-b-[#C5C5C5] rounded-none ps-0 placeholder:text-black focus:outline-none placeholder:font-medium" />
+                        <input type="email" name="email" placeholder="Email" className="input input-bordered border-t-0 border-e-0 border-s-0 border-b-[#C5C5C5] rounded-none ps-0 placeholder:text-black focus:outline-none placeholder:font-medium" required />
                     </div>
+                    {/* date */}
                     <div className="form-control">
-                        <input type="date" placeholder="Date" className="input input-bordered border-t-0 border-e-0 border-s-0 border-b-[#C5C5C5] rounded-none ps-0 placeholder:text-black focus:outline-none placeholder:font-medium" />
+                        <input type="date" name="date" placeholder="Date" className="input input-bordered border-t-0 border-e-0 border-s-0 border-b-[#C5C5C5] rounded-none ps-0 placeholder:text-black focus:outline-none placeholder:font-medium" required />
                     </div>
+                    {/* description */}
                     <div className="form-control">
-                        <input type="text" placeholder="Description" className="input input-bordered border-t-0 border-e-0 border-s-0 border-b-[#C5C5C5] rounded-none ps-0 placeholder:text-black focus:outline-none placeholder:font-medium" />
+                        <input type="text" name="description" placeholder="Description" className="input input-bordered border-t-0 border-e-0 border-s-0 border-b-[#C5C5C5] rounded-none ps-0 placeholder:text-black focus:outline-none placeholder:font-medium" />
                     </div>
+                    {/* organize */}
                     <div className="form-control">
-                        <input type="text" placeholder="Organize books at the library" className="input input-bordered border-t-0 border-e-0 border-s-0 border-b-[#C5C5C5] rounded-none ps-0 placeholder:text-black focus:outline-none placeholder:font-medium" />
+                        <input type="text" name="organize" placeholder="Organize books at the library" className="input input-bordered border-t-0 border-e-0 border-s-0 border-b-[#C5C5C5] rounded-none ps-0 placeholder:text-black focus:outline-none placeholder:font-medium" required />
                     </div>
+                    {/* button */}
                     <div className="form-control mt-6">
                         <button className="btn bg-[#3F90FC] text-white border-none capitalize font-medium text-base rounded-none">Registration</button>
                     </div>
                 </form>
             </div>
+            <ToastContainer />
         </div>
     );
 };

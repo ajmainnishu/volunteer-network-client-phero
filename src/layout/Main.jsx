4ffcaf2f51a9950a0231b77d6c0ef9f32b42dd66
Ajmain Nishu <ajmainnishu@gmail.com>
2023-10-14 13:03:19 +0600

    feat: Search component added

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,9 +1,11 @@
+import { Outlet } from "react-router-dom";
 import Navigation from "../components/Home/Navigation/Navigation";
 
 const Main = () => {
     return (
         <div>
             <Navigation />
+            <Outlet />
         </div>
     );
 };

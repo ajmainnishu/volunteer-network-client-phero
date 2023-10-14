8a23d95dde96330515decc5612029919fa480504
Ajmain Nishu <ajmainnishu@gmail.com>
2023-10-15 01:19:54 +0600

    feat: Events component added

--- src/components/Home/Events/Events.jsx
+++ src/components/Home/Events/Events.jsx
@@ -1,7 +1,70 @@
+import { useEffect, useState } from "react";
+import Swal from 'sweetalert2';
+import { ToastContainer, toast } from 'react-toastify';
+
 const Events = () => {
+    // events get fetch state
+    const [events, setEvents] = useState([]);
+    // get event fetch data from server
+    useEffect(() => {
+        fetch('http://localhost:5000/events')
+            .then(res => res.json())
+            .then(data => setEvents(data))
+    }, [events])
+    // cancel button and delete from server
+    const handleEventDelete = id => {
+        // sweetalert
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, cancel it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                Swal.fire(
+                    'Cancel!',
+                    'Your file has been cancel.',
+                    'success'
+                )
+                fetch(`http://localhost:5000/events/${id}`, {
+                    method: 'DELETE'
+                })
+                    .then().catch(error => {
+                        // toastify
+                        toast(error.message);
+                    })
+            }
+
+        })
+    }
     return (
-        <div>
-            
+        <div className="w-11/12 md:w-9/12 mx-auto pt-3 pb-64">
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-7">
+                {/* events mapping */}
+                {
+                    events.map(event => <div key={event._id} className="grid grid-cols-5 md:space-x-6 space-y-6 md:space-y-0 p-6 rounded-xl" style={{ boxShadow: '0px 10px 30px rgba(0, 0, 0, 0.05)' }}>
+                        {/* image */}
+                        <div className="col-span-5 md:col-span-2">
+                            <img src={event.img} alt="image" />
+                        </div>
+                        {/* info */}
+                        <div className="col-span-5 md:col-span-3 flex flex-col justify-between">
+                            <div className="space-y-3">
+                                <h3 className="text-[#0B0B0B] text-2xl font-bold font-avenir">{event.title}</h3>
+                                <p className="font-semibold text-xl">{event.date}</p>
+                            </div>
+                            {/* button */}
+                            <div className="text-end">
+                                <button onClick={() => handleEventDelete(event._id)} className="btn capitalize text-[#787878] text-base font-medium bg-[#E3E3E3] px-10 border-0 rounded-md">Cancel</button>
+                            </div>
+                        </div>
+                    </div>)
+                }
+            </div>
+            <ToastContainer />
         </div>
     );
 };
--- src/components/Home/Navigation/Navigation.jsx
+++ src/components/Home/Navigation/Navigation.jsx
@@ -11,9 +11,9 @@ const Navigation = () => {
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </label>
                         <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                            <li><Link>Home</Link></li>
+                            <li><Link to={`/`}>Home</Link></li>
                             <li><Link>Donation</Link></li>
-                            <li><Link>Events</Link></li>
+                            <li><Link to={`/events`}>Events</Link></li>
                             <li><Link>Blog</Link></li>
                         </ul>
                     </div>
@@ -28,9 +28,9 @@ const Navigation = () => {
                 <div className="navbar-end space-x-8">
                     <div className="navbar-center hidden lg:flex">
                         <ul className="menu menu-horizontal text-base text-[#0B0B0B] font-medium px-1">
-                            <li><Link>Home</Link></li>
+                            <li><Link to={`/`}>Home</Link></li>
                             <li><Link>Donation</Link></li>
-                            <li><Link>Events</Link></li>
+                            <li><Link to={`/events`}>Events</Link></li>
                             <li><Link>Blog</Link></li>
                         </ul>
                     </div>
